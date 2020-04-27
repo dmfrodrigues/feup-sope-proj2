74626eed3b5bea46850e26c2263430db027073c5
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-04-28 00:10:19 +0100

    implemented atomic stuff

---
+++ common/include/common_atomic.h
@@ -0,0 +1,10 @@
+#ifndef COMMON_ATOMIC_H_INCLUDED
+#define COMMON_ATOMIC_H_INCLUDED
+
+typedef struct atomic_lli atomic_lli_t;
+atomic_lli_t* atomic_lli_ctor(void);
+void atomic_lli_dtor(atomic_lli_t *p);
+void atomic_lli_set(atomic_lli_t *p, long long int n);
+long long int atomic_lli_get(atomic_lli_t *p);
+
+#endif //COMMON_ATOMIC_H_INCLUDED
---
+++ common/src/common_atomic.c
@@ -0,0 +1,30 @@
+#include "common_atomic.h"
+
+#include <pthread.h>
+
+struct atomic_lli {
+    long long int n;
+    pthread_mutex_t mutex;
+};
+atomic_lli_t* atomic_lli_ctor(void){
+    atomic_lli_t *res = malloc(sizeof(atomic_lli_t));
+    res->n = 0;
+    pthread_mutex_init(&res->mutex, NULL);
+    return res;
+}
+void atomic_lli_dtor(atomic_lli_t *p){
+    pthread_mutex_destroy(&p->mutex);
+    free(p);
+}
+void atomic_lli_set(atomic_lli_t *p, long long int n){
+    pthread_mutex_lock(&p->mutex);
+    p->n = n;
+    pthread_mutex_lock(&p->mutex);
+}
+long long int atomic_lli_get(atomic_lli_t *p){
+    long long int n;
+    pthread_mutex_lock(&p->mutex);
+    n = p->n;
+    pthread_mutex_lock(&p->mutex);
+    return n;
+}
