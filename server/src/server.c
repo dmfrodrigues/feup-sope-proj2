1443dc289b322db20e3fca9a8047a2cec92d86a6
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-05-12 20:14:48 +0100

    Tidying up server

--- server/src/server.c
+++ server/src/server.c
@@ -18,57 +18,40 @@
 
 #define MAX_THREADS 1000000
 
-server_args_t args;
-
-int init(int argc, char* argv[]){
-    timeup_server = false;
-    if(server_args_ctor(&args, argc, argv, 1000000)) return EXIT_FAILURE;
-    if(server_install_handlers()) return EXIT_FAILURE;
-    if(server_threads_init()) return EXIT_FAILURE;
-    return EXIT_SUCCESS;
-}
-
-void cleanup(void){
-    if(server_args_dtor(&args)){
-        const char *buf = "Could not destruct args\n";
-        write(STDERR_FILENO, buf, strlen(buf));
-    }
-    if(server_threads_clean()){
-        const char *buf = "Could not clean server threads\n";
-        write(STDERR_FILENO, buf, strlen(buf));
-    }
-}
+server_args_t args;                                                                 // Arguments structure
 
 int main(int argc, char *argv[]){
     int ret = EXIT_SUCCESS;
-
-    if(atexit(cleanup)) return EXIT_FAILURE;
-    if(init(argc, argv)) return EXIT_FAILURE;
-
-    if(alarm(args.nsecs)) return EXIT_FAILURE;
-    
-    mkfifo(args.fifoname, 0660);
-    
-    
-    message_t m;
+    // Initialize
+    timeup_server = false;                                                          // Initialize timeup_server
+    if(server_args_ctor(&args, argc, argv, 1000000))    return EXIT_FAILURE;        // Construct arguments structure from argc/argv
+    if(server_threads_init())                           return EXIT_FAILURE;        // Initialize threads
+    if(server_install_handlers())                       return EXIT_FAILURE;        // Install alarm handler
+    if(alarm(args.nsecs))                               return EXIT_FAILURE;        // Register alarm
+    if(mkfifo(args.fifoname, 0660))                     return EXIT_FAILURE;        // Create public FIFO
+    // Read requests
+    message_t message;
     while(!timeup_server){
         // Open public fifo
-        int fifo_des = open(args.fifoname, O_RDONLY);
-        if(fifo_des == -1){
+        int public_fifo_filedes = open(args.fifoname, O_RDONLY);
+        if(public_fifo_filedes == -1){
             if(errno == EINTR) break;
             else               return EXIT_FAILURE;
         }
         // Read messages
         int r;
-        while((r = read(fifo_des, &m, sizeof(message_t))) == sizeof(message_t)){
-            if(output(&m, op_RECVD)){ ret = EXIT_FAILURE; break; }
-            if(server_create_thread(&m)){ ret = EXIT_FAILURE; break; }
+        while((r = read(public_fifo_filedes, &message, sizeof(message_t))) == sizeof(message_t)){
+            if(output(&message, op_RECVD)){ ret = EXIT_FAILURE; break; }
+            if(server_create_thread(&message)){ ret = EXIT_FAILURE; break; }
         }
         // Close public fifo
-        close(fifo_des);
+        close(public_fifo_filedes);
     }
-    
+
     if (server_close_service(args.fifoname)) ret = EXIT_FAILURE;
+    // Cleanup
+    if(server_args_dtor(&args)) return EXIT_FAILURE;
+    if(server_threads_clean())  return EXIT_FAILURE;
 
     return ret;
 }
