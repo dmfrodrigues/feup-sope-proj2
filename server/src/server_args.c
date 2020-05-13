479d4d2774ba6d7a65db01f5ecfae4baf692e050
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-05-13 14:47:22 +0100

    More changes

--- server/src/server_args.c
+++ server/src/server_args.c
@@ -28,25 +28,23 @@ int server_args_ctor(server_args_t *p, int argc, char *argv[], int max_threads, 
     // Cycle through arguments
     optind = 1;
     int opt;
-    while((opt = getopt(argc, argv, OPTSTRING)) != -1){
+    while((opt = getopt(argc, argv, OPTSTRING)) != -1){                                         // While there are options to process
         switch(opt){
             case 't': if(sscanf(optarg, "%d", &p->nsecs   ) != 1) return EXIT_FAILURE; break;   // Time
             case 'l': if(sscanf(optarg, "%d", &p->nplaces ) != 1) return EXIT_FAILURE; break;   // Number of places
             case 'n': if(sscanf(optarg, "%d", &p->nthreads) != 1) return EXIT_FAILURE; break;   // Number of threads
-            case '?':
-                return EXIT_FAILURE;
-            default:
-                optind = 1; return EXIT_FAILURE;
+            case '?':                                             return EXIT_FAILURE;          // Unknown characters
+            default: optind = 1;                                  return EXIT_FAILURE;          // Other errors
         }
     }
 
-    if (p->nsecs    == SERVER_ARGS_DEFAULT.nsecs    ||
-        p->nthreads > max_threads ||
-        argc - optind != 1){
+    if (p->nsecs    == SERVER_ARGS_DEFAULT.nsecs    ||                                          // If time was not set, or
+        p->nthreads > max_threads ||                                                            // if the number of threads is larger than the max number of threads, or
+        argc - optind != 1){                                                                    // if there are more than one arguments that are not options
         errno = EINVAL;
         return EXIT_FAILURE;
     }
-
+    // Get fifoname
     p->fifoname = calloc(strlen(argv[optind])+strlen(FIFO_PREFIX)+1, sizeof(char));
     strcat(strcpy(p->fifoname, FIFO_PREFIX), argv[optind]);
 
