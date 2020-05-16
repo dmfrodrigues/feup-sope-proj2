ab9567149d31abfe96c6d55904026fafb661502d
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-05-16 23:08:55 +0100

    More tests, failing 09

--- test/test.sh
+++ test/test.sh
@@ -7,12 +7,12 @@ RED_INTENSIVE='\033[1;31m'
 NC_INTENSIVE='\033[1m' # No Color
 NC='\033[0m' # No Color
 
-test/test1.sh
-test/test2.sh
-test/test3.sh
-test/test4.sh
-test/test5.sh
-test/test6.sh
-test/test7.sh
-test/test8.sh
-test/test9.sh
+test/test01.sh
+test/test02.sh
+test/test03.sh
+test/test04.sh
+test/test05.sh
+test/test06.sh
+test/test07.sh
+test/test08.sh
+test/test09.sh
---
+++ test/test09.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+QLOG=Q.log
+ULOG=U.log
+QERR=Q.err
+UERR=U.err
+
+(timeout 4 ./Q2 -t 3 -l 5 fifinho > $QLOG 2> $QERR; echo $? >> $QERR) &
+(timeout 3 ./U2 -t 2           fifinho > $ULOG 2> $UERR; echo $? >> $UERR) &
+sleep 5
+test/aval1.sh $ULOG $QLOG
+test/eval-stderr.sh $UERR $QERR
